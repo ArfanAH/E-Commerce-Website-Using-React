c0c8a27250e1c2340e2de72d0921da9394261dd9
ArfanAH <arfinarfan000@gmail.com>
2024-11-16 14:06:11 +0600

    update

--- src/item-details/Description.jsx
+++ src/item-details/Description.jsx
@@ -5,8 +5,8 @@ function Description({itemDetail}) {
     <div>
       {itemDetail?.description ?
         <div className='p-5 rounded-xl bg-white shadow-md mt-6 border'>
-          <h2 className='my-2 font-medium text-2xl'>Description</h2>
-          <p>{itemDetail?.description}</p>
+          <h2 className='my-2 font-medium text-md md:text-2xl'>Description</h2>
+          <p className='text-sm md:text-md'>{itemDetail?.description}</p>
         </div> :
         <div className='w-full h-[100px] mt-7 bg-slate-300 animate-pulse rounded-xl'>
 
--- src/item-details/ImageGallery.jsx
+++ src/item-details/ImageGallery.jsx
@@ -4,7 +4,7 @@ function ImageGallery({itemDetail}) {
   return (
     <div>
       <img src={itemDetail?.images[0]}
-        className='w-full h-[500px] object-cover rounded-xl lg:object-scale-down'
+        className='w-full h-[200px] md:h-[500px] object-cover rounded-xl lg:object-scale-down'
       />
     </div>
   )
